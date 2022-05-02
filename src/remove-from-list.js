c2fa16736e14346978610507717e4e98a2013bab
vnln <bananiqus@gmail.com>
2022-05-03 01:32:28 +0400

    feat: add remove-from-list

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -22,11 +22,13 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  if (l.value === k) l = l.next;
+  let curr = l;
+  while (curr.next) curr.next.value === k ? (curr.next = curr.next.next) : (curr = curr.next);
+  return l;
 }
 
 module.exports = {
-  removeKFromList
+  removeKFromList,
 };
